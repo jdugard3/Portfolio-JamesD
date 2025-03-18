9c5f1e7c164ff7b950299798a394a0b5c9703ee5
James Dugard <jdugard3@gmail.com>
2025-03-17 21:55:31 -0400

    finalized color scheme and began working on about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">James Dugard </span>
+            from <span className="purple">Miami, FL.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently searching for a job in the field of software development.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed a Full Stack Development Bootcamp at 4Geeks Academy.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Music
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Being a Dad & Husband!
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "You gain the most experience when you make mistakes!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">James Dugard (Or someone else)</footer>
         </blockquote>
       </Card.Body>
     </Card>
